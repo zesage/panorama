e74263b3fb860ae668aa3ee227a3c932b7e7753e
Mark Hu <zesage@gmail.com>
2020-02-18 21:43:38 +0800

    initial

---
+++ example/lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:panorama/panorama.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Panorama',
+      theme: ThemeData.dark(),
+      home: MyHomePage(title: 'Flutter Panorama'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Panorama(),
+    );
+  }
+}
---
+++ lib/panorama.dart
@@ -0,0 +1,15 @@
+library panorama;
+
+import 'package:flutter/material.dart';
+
+class Panorama extends StatefulWidget {
+  @override
+  _PanoramaState createState() => _PanoramaState();
+}
+
+class _PanoramaState extends State<Panorama> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
